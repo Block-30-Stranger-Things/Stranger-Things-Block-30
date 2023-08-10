455edaeb4dfcbe8580b9bdd747b803d3badcc131
Vonney <davonnejv@gmail.com>
2023-08-10 02:57:38 -0500

    adding navigate to add new post button

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { fetchAllPosts } from './API';
 import SearchBar from './SearchBar';
 import './Posts-Profile.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
@@ -24,18 +25,30 @@ export default function Posts() {
     post.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  //navigate
+  const navigate = useNavigate();
+  
+  function navigateToNewPost() {
+    navigate('./newPost')
+  }
+
   return (
     <div className="posts-container">
       <h1>Posts</h1>
       <p className="search">Search for Posts: </p>
       <SearchBar value={searchQuery} onChange={handleSearchInput}/>
 
+      <button className="newPost" onClick={navigateToNewPost}>Add New Post</button>
+
       {filteredPosts.map((post) => (
         <div key={post._id}>
           <h2>Title: {post.title}</h2>
           <p>Description: {post.description}</p>
           <p>Price: {post.price}</p>
+          <p>Location: {post.location}</p>
           <p>Created At: {post.createdAt}</p>
+
+
           <p></p>
           <hr />
         </div>
