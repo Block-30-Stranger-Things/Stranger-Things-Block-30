c1babde9f0955ccfd8508c5cda4aeedfc70fd57d
Vonney <davonnejv@gmail.com>
2023-08-09 23:20:56 -0500

    adding styles and h1 to home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './components/Home'
 import Login from './components/Login'
 import Profile from './components/Profile'
 import Posts from './components/Posts'
-import SignUp from './components/SignUp'
+import Register from './components/Register'
 import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
@@ -11,25 +11,23 @@ function App() {
   return (
   <>
     <div className="link-container">
-      <div className="myLinks">
+      <div className="navigation">
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/posts">Posts</Link>
         <Link to="/profile">Profile</Link>
-        <Link to="/signUp">Sign Up</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </div>
 
-    
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/posts" element={<Posts />} />
       <Route path="/profile" element={<Profile/>} />
-      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signUp" element={<Register />} />
     </Routes>
   </>
-  
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Block 30: Career Simulation: Stranger’s Things </h1>
+    </div>
   )
 }
