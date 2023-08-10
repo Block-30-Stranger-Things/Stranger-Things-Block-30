3afdb619f1a0352cf00ab275630a2cfc327520e1
Vonney <davonnejv@gmail.com>
2023-08-09 23:55:09 -0500

    adding css to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
+import './Home-Login.css'
 
 export default function Login() {
+const [userName, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+
+
   return (
-    <div>Login</div>
+    <>
+      <div className="login-container">
+        <h2>Log In</h2>
+        <form>
+          <label>
+            Username: 
+            <input value={userName} 
+            placeholder="Username"/>
+          </label>
+
+          <label>
+           Password: 
+            <input 
+            value={password} 
+            type="password"
+            placeholder="Enter Password"/>
+          </label>
+
+          <button className="login-btn">
+            Login
+          </button>
+      </form>
+
+
+
+
+      </div>
+    </>
   )
 }
