ae57d886e35d4064551a32a3d5f12de042486889
Vonney <davonnejv@gmail.com>
2023-08-10 14:28:55 -0500

    trying to get login to work, I set up the function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
+import { login } from './API'
 import './Home-Login.css'
 
 export default function Login() {
 const [userName, setUsername] = useState("");
 const [password, setPassword] = useState("");
+const [token, setToken] = useState("");
 
 const inputtingUsername = (e) => {
     setUsername(e.target.value);
@@ -17,6 +19,32 @@ const inputtingPassword = (e) => {
 const handelSubmit = (e) => {
     e.preventDefault();
 }
+
+useEffect(() => {
+    if (userName && password) {
+      const getLogin = async () => {
+        try {
+          const result = await login(userName, password);
+  
+          if (result.success) {
+            console.log("Login is successful")
+
+            const token = result.data.token
+            setToken(token);
+
+          } else {
+            console.log("Login failed. Incorrect username or password. Please try again");
+            
+          }
+        } catch (err) {
+          console.error(err);
+          
+        }
+      }
+  
+      getLogin();
+    }
+  }, [userName, password]);
 
   return (
     <>
