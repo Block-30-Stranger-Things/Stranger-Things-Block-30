1f90682f36a2e1af4d0b12ec834d7eea390cbf76
Vonney <davonnejv@gmail.com>
2023-09-22 22:45:07 -0500

    hadduplicate code due to merges

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -34,17 +34,16 @@ export default function Profile({ token, userName }) {
 
   //get todays date
   function getDate() {
-    document.getElementById("getTodaysDate").innerHTML = Date();
+    const dateElement = document.getElementById("getTodaysDate");
+    if (dateElement) {
+      dateElement.innerHTML = new Date().toString();
+    } else {
+      console.error("Element with id 'getTodaysDate' not found");
+    }
   }
-
-//get todays date
-function getDate() {
-  document.getElementById("getTodaysDate").innerHTML = Date();
-}
 
   return (
     <>
-    
       {token ? (
         <div className="profile-container">
           <h2 className="profile-heading">My Profile</h2>
@@ -71,23 +70,6 @@ function getDate() {
           </button>
         </div>
       )}
-
-     <div className="profile-container">
-      <h2 className="profile-heading">My Profile</h2>
-      <p id="getTodaysDate"></p>
-      <button onClick={getDate} className="date">Todays Date</button>
-
-     <div className="profile-info">  
-       <p>Welcome, {profile.username}!</p>
-       <p>Messages: TBD Will Work On This {profile.messages}</p>
-       <p>ID: {profile._id}</p>  
-       <p>Cohort: {profile.cohort}</p>
-       <button className="logOut" type="button" onClick={logOut}>Log Out</button>
-
-      <hr />
-      <MyPosts posts={posts} />
-      </div>
-   </div>
-  </>
-  )
+    </>
+  );
 }
